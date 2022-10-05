b65fd6898fcf7eb5a2a443f9ec6e67d8e645223e
maocprogramm <93774009+maocprogramm@users.noreply.github.com>
2022-10-05 13:22:37 -0400

    ajoutSuccursalesActivity

---
+++ app/src/main/java/ca/qc/cgodin/mini_projet3/SuccursalesActivity.kt
@@ -0,0 +1,11 @@
+package ca.qc.cgodin.mini_projet3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuccursalesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_succursales)
+    }
+}
