12be43a37a89c6ba82b6a4912d485dba941b9686
maocprogramm <93774009+maocprogramm@users.noreply.github.com>
2022-10-06 14:41:42 -0400

    Update SuccursalesActivity.kt

--- app/src/main/java/ca/qc/cgodin/mini_projet3/SuccursalesActivity.kt
+++ app/src/main/java/ca/qc/cgodin/mini_projet3/SuccursalesActivity.kt
@@ -2,10 +2,43 @@ package ca.qc.cgodin.mini_projet3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import ca.qc.cgodin.mini_projet3.ViewModel.SucViewModel
+import ca.qc.cgodin.mini_projet3.ViewModel.SucViewModelProviderFactory
+import ca.qc.cgodin.mini_projet3.databinding.ActivityMainBinding
+import ca.qc.cgodin.mini_projet3.databinding.ActivitySuccursalesBinding
+import ca.qc.cgodin.mini_projet3.repository.SucRepository
 
 class SuccursalesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySuccursalesBinding
+    private lateinit var sucViewModel: SucViewModel
+
+    private val navController by lazy {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navHostFragment.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_succursales)
+        binding = ActivitySuccursalesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        val sucRepository = SucRepository()
+        val viewModelProviderFactory = SucViewModelProviderFactory(sucRepository)
+
+        try {
+            val viewModelProvider = ViewModelProvider(
+                navController.getViewModelStoreOwner(R.id.nav_graph), viewModelProviderFactory
+            )
+            sucViewModel = viewModelProvider.get(SucViewModel::class.java)
+
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+        }
     }
 }
