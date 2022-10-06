87c62739db5d5128077a7bfaef124ca4f16f08a2
maocprogramm <93774009+maocprogramm@users.noreply.github.com>
2022-10-06 14:43:35 -0400

    Update ConnexionFragment.kt

--- app/src/main/java/ca/qc/cgodin/mini_projet3/ConnexionFragment.kt
+++ app/src/main/java/ca/qc/cgodin/mini_projet3/ConnexionFragment.kt
@@ -1,11 +1,14 @@
 package ca.qc.cgodin.mini_projet3
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import ca.qc.cgodin.mini_projet3.R
+import ca.qc.cgodin.mini_projet3.databinding.FragmentConnexionBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,8 @@ class ConnexionFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentConnexionBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,8 @@ class ConnexionFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
 
     override fun onCreateView(
@@ -35,7 +42,21 @@ class ConnexionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connexion, container, false)
+        _binding = FragmentConnexionBinding.inflate(inflater, container, false)
+        val view = binding.root
+        binding.btnConnexion.setOnClickListener{
+            if (binding.etID.text.toString().equals("1111111") && binding.etPwd.text.toString().equals("Password1")){
+                val intent = Intent(context, SuccursalesActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                val toast = Toast.makeText(context, resources.getString(R.string.toast_conn_refus), Toast.LENGTH_SHORT)
+                toast.show()
+            }
+
+        }
+
+        return view
     }
 
     companion object {
